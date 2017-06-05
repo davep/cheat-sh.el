ddd8c67bd83f51a9972ba2055c16a0b7e160d1b6
Dave Pearson <davep@davep.org>
2017-06-05 16:22:10 +0100

    v1.2

--- cheat-sh.el
+++ cheat-sh.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: docs, help
 ;; URL: https://github.com/davep/cheat-sh.el
 ;; Package-Requires: ((emacs "24"))
