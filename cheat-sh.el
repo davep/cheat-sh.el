0d3835c23e637c5f7a8eef3ef3ac8ae72bac07f6
Dave Pearson <davep@davep.org>
2017-06-15 10:52:23 +0100

    Let sheet list go "stale" so it gets reloaded in long Emacs sessions

--- cheat-sh.el
+++ cheat-sh.el
@@ -27,6 +27,11 @@
   "Face used on captions in the cheat-sh output window."
   :group 'cheat-sh)
 
+(defcustom cheat-sh-list-timeout (* 60 60 4)
+  "Seconds to wait before deciding the cached sheet list is \"stale\"."
+  :type 'integer
+  :group 'cheat-sh)
+
 (defconst cheat-sh-url "http://cheat.sh/%s?T"
   "URL for cheat.sh.")
 
@@ -53,15 +58,30 @@ text.")
 (defvar cheat-sh-sheet-list nil
   "List of all available sheets.")
 
+(defvar cheat-sh-sheet-list-acquired nil
+  "The time when `cheat-sh-sheet-list' was populated.")
+
+(defun cheat-sh-sheet-list-cache ()
+  "Return the list of sheets.
+
+The list is cached in memory, and is considered \"stale\" and is
+refreshed after `cheat-sh-list-timeout' seconds."
+  (when (and cheat-sh-sheet-list-acquired
+             (> (- (time-to-seconds) cheat-sh-sheet-list-acquired) cheat-sh-list-timeout))
+    (message "Making list stale")
+    (setq cheat-sh-sheet-list nil))
+  (or cheat-sh-sheet-list
+      (progn
+        (setq cheat-sh-sheet-list-acquired (time-to-seconds))
+        (setq cheat-sh-sheet-list (split-string (cheat-sh-get ":list") "\n")))))
+
 (defun cheat-sh-read (prompt)
   "Read input from the user, showing PROMPT to prompt them.
 
 This function is used by some `interactive' functions in
 cheat-sh.el to get the item to look up. It provides completion
 based of the sheets that are available on cheat.sh."
-  (completing-read prompt
-                   (or cheat-sh-sheet-list
-                       (setq cheat-sh-sheet-list (split-string (cheat-sh-get ":list") "\n")))))
+  (completing-read prompt (cheat-sh-sheet-list-cache)))
 
 (defun cheat-sh-decorate-results (buffer)
   "Decorate BUFFER with properties to highlight results."
